88f4f9296bf6f181f2d694efb951b8a6e16923af
gonchar44 <artemgonchar7@gmail.com>
2022-02-20 18:19:18 +0200

    delete HelloWorld & create Post component, import VueCompositionApi & Vue2TouchEvents

---
+++ src/components/Post.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="post-c">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+}
+</script>
+
+<style>
+  .post-c {
+    width: 320px;
+    border-radius: 8px;
+    box-shadow: 0 0 11px 5px rgba(0, 0, 0, .29);
+    margin: 10px auto;
+    padding: 10px 0;
+  }
+</style>
